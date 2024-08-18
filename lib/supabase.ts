42ee14f2aae14de118e84f212fb984883187bab4
Arvin Ayson <a.ayson12345@gmail.com>
2024-08-18 12:05:55 +0800

    fix exporting

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Using the environment variables
-const supabaseServiceRole = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5cHRicmZ4bmV3eXp2cnN3emlmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMDEyNzcyMywiZXhwIjoyMDI1NzAzNzIzfQ.pSrxBMURKGDm4NZ5jBR-6xUgZ_pyoySdTE4AEC19ZsI"
-const supabaseUrl = "https://dyptbrfxnewyzvrswzif.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5cHRicmZ4bmV3eXp2cnN3emlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAxMjc3MjMsImV4cCI6MjAyNTcwMzcyM30.4XfIFo3UGHJG_5oxwAeGUcaYmM72E_LfyvB2O0o-1nE";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-if (!supabaseUrl || !supabaseKey || !supabaseServiceRole) {
-  throw new Error('Missing environment variables for Supabase');
-}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
